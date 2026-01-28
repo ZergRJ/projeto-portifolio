6fe0fec4200ea7827c7e4d0614fda0579c87d27a
Gean Lucas <geanlxr@gmail.com>
2026-01-28 02:26:30 -0300

    Comitando o começo do projeto

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.menu-btn');
+const sidebar = document.querySelector('.sidebar');
+
+btn.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
